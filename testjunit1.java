069e1fc4e695f5df83466f0ad820950fc565a5cf
Lektorfuja <59904852+Lektorfuja@users.noreply.github.com>
2023-07-01 17:51:56 +0300

    Add files via upload

---
+++ testjunit1.java
@@ -0,0 +1,11 @@
+package test1;
+
+import junit.framework.TestCase;
+
+public class testjunit1 extends TestCase {
+	public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+
+}
+}
